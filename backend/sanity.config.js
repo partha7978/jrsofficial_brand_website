25d3071a927057a7184ae3e30847a0567939a80b
Partha Sarathi Muduli <66170183+partha7978@users.noreply.github.com>
2024-12-11 00:53:59 +0530

    added vercel plugin

--- backend/sanity.config.js
+++ backend/sanity.config.js
@@ -2,6 +2,7 @@ import {defineConfig} from 'sanity'
 import {structureTool} from 'sanity/structure'
 import {visionTool} from '@sanity/vision'
 import {schemaTypes} from './schema'
+import { vercelDeployTool } from 'sanity-plugin-vercel-deploy'
 
 export default defineConfig({
   name: 'default',
@@ -10,7 +11,7 @@ export default defineConfig({
   projectId: 'lnbqi1qv',
   dataset: 'production',
 
-  plugins: [structureTool(), visionTool()],
+  plugins: [structureTool(), visionTool(), vercelDeployTool()],
 
   schema: {
     types: schemaTypes,
