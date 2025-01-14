47a7b2947bc0dbfa4ebe9aea7fd0c56fdcaa6228
Partha Sarathi Muduli <66170183+partha7978@users.noreply.github.com>
2025-01-15 00:37:12 +0530

    added episodes schema

---
+++ backend/schema/episodes.js
@@ -0,0 +1,72 @@
+import { getExtension } from '@sanity/asset-utils'
+
+export default {
+  name: 'episodes',
+  title: 'Episodes',
+  type: 'document',
+  fields: [
+    {
+      name: 'title',
+      title: 'Title',
+      type: 'string',
+    },
+    {
+      name: 'category',
+      title: 'Category',
+      type: 'string',
+    },
+    {
+      name: 'relatedTags',
+      title: 'relatedTags',
+      type: 'array',
+      of: [
+        {
+          name: 'tag',
+          title: 'Tag',
+          type: 'string',
+        },
+      ],
+    },
+    {
+      name: 'speakerName',
+      title: 'Speaker Name',
+      type: 'string',
+    },
+    {
+      name: 'episodeDate',
+      title: 'Episode Date',
+      type: 'datetime',
+    },
+    {
+      name: 'episodeImage',
+      title: 'Episode Image',
+      type: 'image',
+      options: {
+        hotspot: true,
+      },
+      validation: (Rule) =>
+        Rule.custom((image) => {
+          if (!image || !image.asset) return true;
+          const extension = getExtension(image.asset._ref)
+          return extension === 'webp'
+            ? true
+            : 'Only .webp format images are allowed.';
+        }),
+    },
+    {
+      name: 'episodeAudio',
+      title: 'Episode Audio',
+      type: 'file',
+    },
+    {
+      name: 'content',
+      type: 'array',
+      title: 'Content',
+      of: [
+        {
+          type: 'block',
+        },
+      ],
+    },
+  ],
+}
--- backend/schema/index.js
+++ backend/schema/index.js
@@ -1,4 +1,5 @@
 import navigationBar from "./navigationBar"
 import homePage from "./homePage"
+import episodes from "./episodes"
 
-export const schemaTypes = [navigationBar, homePage]
+export const schemaTypes = [navigationBar, homePage, episodes]
