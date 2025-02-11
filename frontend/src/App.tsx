fb698427e2368cd9dddfed9fdbec704838f8d143
Partha Sarathi Muduli <66170183+partha7978@users.noreply.github.com>
2025-02-12 00:04:48 +0530

    added smooth scrolling across page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,10 +3,13 @@ import { Home, Episodes, About } from "./containers";
 import { Navbar } from "./components";
 import { Provider } from "react-redux";
 import dataStore from "./store/dataStore";
+import Scroll from './components/Smoothscroll/SmoothScroll.js';
+
 
 function App() {
   return (
     <>
+    <Scroll />
       <Provider store={dataStore}>
         <Navbar />
         <Home />
