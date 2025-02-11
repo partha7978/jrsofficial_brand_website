35c5682ab5e552f810ce8c7efe639692c340dfd9
Partha Sarathi Muduli <66170183+partha7978@users.noreply.github.com>
2025-02-12 00:14:17 +0530

    reverting smooth scroll

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,13 +3,10 @@ import { Home, Episodes, About } from "./containers";
 import { Navbar } from "./components";
 import { Provider } from "react-redux";
 import dataStore from "./store/dataStore";
-import Scroll from './components/Smoothscroll/SmoothScroll.js';
-
 
 function App() {
   return (
     <>
-    <Scroll />
       <Provider store={dataStore}>
         <Navbar />
         <Home />
