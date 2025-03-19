5341edf482e0fd10d1d9dd6b3e30c59290485092
Partha Sarathi Muduli <66170183+partha7978@users.noreply.github.com>
2025-03-19 23:13:06 +0530

    Added testimonial in contact Page

--- frontend/src/containers/Contact/Contact.tsx
+++ frontend/src/containers/Contact/Contact.tsx
@@ -7,6 +7,7 @@ import useFetchData from "../../hooks/useFetchData";
 import { ContactSchema } from "../../interfaces/interface";
 import Loader from "../../components/Loader/Loader";
 import Footer from "../Footer/Footer";
+import Testimonials from "../Testimonials/Testimonials";
 
 const Contact = () => {
   const [isFocused, setIsFocused] = useState(false);
@@ -33,77 +34,82 @@ const Contact = () => {
       {error && <h1>Something went wrong</h1>}
       {loading && <Loader />}
       {contactData && (
-        <div className="contact-container">
-          <section className="contact">
-            <div className="contact-data">
-              <div className="contact-data-title">
-                <h1>{contactData.title}</h1>
-              </div>
-              <div className="contact-data-subtext">
-                <span>{contactData.description}</span>
-              </div>
-              <div className="contact-data-form">
-                <form>
-                  <div
-                    className="contact-data-form-item"
-                    style={{
-                      borderBottom: isFocused
-                        ? "2px solid #FFCA85"
-                        : "2px solid rgba(194, 194, 194, 0.5)",
-                    }}
-                  >
-                    <label
+        <div className="contact-section-wrapper">
+          <div className="contact-container">
+            <section className="contact">
+              <div className="contact-data">
+                <div className="contact-data-title">
+                  <h1>{contactData.title}</h1>
+                </div>
+                <div className="contact-data-subtext">
+                  <span>{contactData.description}</span>
+                </div>
+                <div className="contact-data-form">
+                  <form>
+                    <div
+                      className="contact-data-form-item"
                       style={{
-                        top: isFocused || value ? "-8px" : "20%",
-                        fontSize: isFocused || value ? "12px" : "16px",
-                        color: isFocused
-                          ? "#FFCA85"
-                          : "rgba(194, 194, 194, 0.5)",
+                        borderBottom: isFocused
+                          ? "2px solid #FFCA85"
+                          : "2px solid rgba(194, 194, 194, 0.5)",
                       }}
                     >
-                      Name
-                    </label>
-                    <input
-                      type="text"
-                      value={value}
-                      onChange={(e) => setValue(e.target.value)}
-                      onFocus={() => setIsFocused(true)}
-                      onBlur={() => setIsFocused(value.length > 0)}
-                    />
-                    <button
-                      style={{
-                        color:
-                          isFocused && value.length > 0
+                      <label
+                        style={{
+                          top: isFocused || value ? "-8px" : "20%",
+                          fontSize: isFocused || value ? "12px" : "16px",
+                          color: isFocused
                             ? "#FFCA85"
                             : "rgba(194, 194, 194, 0.5)",
-                      }}
-                      className={`${
-                        isFocused && value.length > 0 ? "bubble-animation" : ""
-                      }`}
-                    >
-                      <TiLocationArrowOutline />
-                    </button>
+                        }}
+                      >
+                        Name
+                      </label>
+                      <input
+                        type="text"
+                        value={value}
+                        onChange={(e) => setValue(e.target.value)}
+                        onFocus={() => setIsFocused(true)}
+                        onBlur={() => setIsFocused(value.length > 0)}
+                      />
+                      <button
+                        style={{
+                          color:
+                            isFocused && value.length > 0
+                              ? "#FFCA85"
+                              : "rgba(194, 194, 194, 0.5)",
+                        }}
+                        className={`${
+                          isFocused && value.length > 0
+                            ? "bubble-animation"
+                            : ""
+                        }`}
+                      >
+                        <TiLocationArrowOutline />
+                      </button>
+                    </div>
+                  </form>
+                </div>
+                <div className="contact-data-link">
+                  <div className="contact-data-link-item">
+                    <MdOutlineMail />
+                    <span>{contactData.email}</span>
                   </div>
-                </form>
-              </div>
-              <div className="contact-data-link">
-                <div className="contact-data-link-item">
-                  <MdOutlineMail />
-                  <span>{contactData.email}</span>
-                </div>
-                <div className="contact-data-link-item">
-                  <FiPhoneCall />
-                  <span>+91 {contactData.phone}</span>
+                  <div className="contact-data-link-item">
+                    <FiPhoneCall />
+                    <span>+91 {contactData.phone}</span>
+                  </div>
                 </div>
               </div>
-            </div>
-          </section>
-          /* FAQ COMPONENT */
+            </section>
+            /* FAQ COMPONENT */ /* TESTIMONIAL COMPONENT */
+          </div>
+          <Testimonials />
         </div>
       )}
     </>
   );
 };
-const ContactWithFooter = Footer(Contact, 'contact-footer');
+const ContactWithFooter = Footer(Contact, "contact-footer");
 
 export default ContactWithFooter;
