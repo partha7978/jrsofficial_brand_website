59f6048b78e9fb5d80ac3f4d2d0d67e3ee3a0851
Partha Sarathi Muduli <66170183+partha7978@users.noreply.github.com>
2025-03-31 01:12:54 +0530

    Added Featured podcast section with form, in course page

--- frontend/src/assets/index.ts
+++ frontend/src/assets/index.ts
@@ -2,3 +2,4 @@ export { default as reactLogo } from "./react.svg";
 export { default as placeholder_image } from "./images/placeholder_img.webp";
 export { default as sliderImgPlaceholder } from "./images/sliderImgPlaceholder.webp";
 export { default as SubscribeSectionImg } from "./images/SubscribeSectionImg.webp";
+export { default as coursePage_img } from "./images/coursePage_img.webp";
--- frontend/src/containers/Course/Course.tsx
+++ frontend/src/containers/Course/Course.tsx
@@ -2,6 +2,7 @@ import "./Course.scss";
 import { IoIosArrowDown } from "react-icons/io";
 import { Button } from "../../components";
 import { FaVideo } from "react-icons/fa6";
+import { coursePage_img } from "../../assets";
 
 const Course = () => {
   return (
@@ -32,6 +33,76 @@ const Course = () => {
         </div>
         <div className="course-video-navigation">
           <IoIosArrowDown />
+        </div>
+      </section>
+      <section className="course-form"></section>
+      <section className="featured-podcast">
+        <div className="featured-podcast-top-section">
+          <div className="featured-podcast-top-section-banner">
+            <div className="img-bg">
+              <img
+                src={coursePage_img}
+                alt="featured podcast banner"
+                loading="lazy"
+                height={200}
+                width={200}
+              />
+            </div>
+          </div>
+          <div className="featured-podcast-top-section-heading">
+            <h2>Expert guidance for your path</h2>
+          </div>
+        </div>
+        <div className="featured-podcast-mobile-separator">
+          <div className="featured-podcast-mobile-separator-line"></div>
+          <div className="featured-podcast-mobile-separator-line"></div>
+        </div>
+        <div className="featured-podcast-bottom-section">
+          <div className="featured-podcast-bottom-section-action">
+            <h2>Learn How to Get Anything You Want</h2>
+            <span className="description">
+              Achieve lasting transformation in all areas of your life. Discover
+              the support you need to do more, be more and serve more with
+              one-on-one strategy sessions with our expert results coaches.
+            </span>
+            <div className="featured-ratio">
+              <div className="ratio">
+                <p>81%</p>
+                <span>People got their Niche Clarity</span>
+              </div>
+              <div className="ratio">
+                <p>72%</p>
+                <span>People got their productivity boost</span>
+              </div>
+            </div>
+            <form action="">
+              <input className="name" type="text" placeholder="First Name" />
+              <input className="email" type="text" placeholder="Email" />
+              <Button
+                name="Send me invitations"
+                backgroundColor="rgb(255, 151, 0)"
+                color="#000000"
+                hoverBackgroundColor="#ffffff"
+                hoverColor="#000000"
+              />
+              {/* <span className="additional-description">
+                FREE access to exclusive insights, private Q+As, and inspiring
+                episodes of The JRS Show, delivered with 💜 to your inbox.
+                (Unsub anytime in a click.)
+              </span> */}
+            </form>
+          </div>
+          <div className="featured-podcast-bottom-section-video">
+            <video
+              src="https://www.w3schools.com/html/mov_bbb.mp4"
+              autoPlay
+              loop
+              muted
+              preload="auto"
+              playsInline
+            ></video>
+            <div className="video-overlay"></div>
+          </div>
         </div>
       </section>
     </main>
