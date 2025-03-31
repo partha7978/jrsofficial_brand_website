4d5056f35665358bd2c11e08760ecf0394b1ab11
Partha Sarathi Muduli <66170183+partha7978@users.noreply.github.com>
2025-03-31 23:36:50 +0530

    Added About section in Course page

--- frontend/src/containers/Course/Course.tsx
+++ frontend/src/containers/Course/Course.tsx
@@ -5,9 +5,22 @@ import { FaVideo } from "react-icons/fa6";
 import { coursePage_img } from "../../assets";
 import { FaPlay } from "react-icons/fa6";
 import Footer from "../Footer/Footer";
+import { Link } from "react-router";
+import { useEffect } from "react";
 
 const Course = () => {
+  useEffect(() => {
+    const rootElement = document.getElementById("root");
+    if (rootElement && rootElement.style) {
+      rootElement.style.background = "#000000";
+    }
 
+    return () => {
+      if (rootElement && rootElement.style) {
+        rootElement.style.background = "#FBF7EC";
+      }
+    };
+  }, []);
   return (
     <main className="course">
       <section className="course-video">
@@ -101,6 +114,47 @@ const Course = () => {
             </div>
           </div>
         </section>
+      </section>
+      <section className="course-about">
+        <div className="course-about-container">
+          <div className="course-about-image-section">
+            <img
+              src="https://images.unsplash.com/photo-1604856420566-576ba98b53cd?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8ODV8fGJsdWUlMjB0aGVtZXxlbnwwfHwwfHx8MA%3D%3D"
+              alt="The JRS Show Image"
+              loading="lazy"
+              height={200}
+              width={200}
+            />
+          </div>
+          <div className="course-about-overlay"></div>
+          <div className="course-about-content">
+            <div className="course-about-content-heading">
+              <p>Heya!</p>
+              <h2>I’m JRS</h2>
+            </div>
+            <span className="course-about-content-description">
+              My hunch is someone you trust mentioned my name, or you stumbled
+              upon one of my videos, quotes or articles online. Whatever path
+              you took, I’m really glad you’re here. This site is full of
+              incredible resources and ideas that can help you change your life
+              (not kidding!). Here’s a quick lay of the land so you can find
+              what you’re looking for and we can start something beautiful
+              together.
+            </span>
+            <div className="course-about-content-action">
+              <Link to="/about">
+                <Button
+                  name="Know More"
+                  backgroundColor="rgb(0, 0, 0, 0.5)"
+                  color="#ffffff"
+                  hoverBackgroundColor="rgb(0, 0, 0, 0.5)"
+                  hoverColor="#ffffff"
+                  // backgroundBlur={42}
+                />
+              </Link>
+            </div>
+          </div>
+        </div>
       </section>
       <section className="featured-podcast">
         <div className="featured-podcast-top-section">
