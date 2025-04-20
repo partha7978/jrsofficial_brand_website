bddcde0ed890827c24010499d046e44b6f27a5a9
Partha Sarathi Muduli <66170183+partha7978@users.noreply.github.com>
2025-04-20 16:49:35 +0530

    Fixed typo error.

--- frontend/src/components/ProdSubPage/index.ts
+++ frontend/src/components/ProdSubPage/index.ts
@@ -2,4 +2,4 @@ export { default as ProductionHomePage } from "./ProductionHomePage";
 export { default as ProductionAboutPage } from "./ProductionAboutPage";
 export { default as ProductionPricePage } from "./ProductionPricePage";
 export { default as ProductionReusuableSeparator } from "./ProductionReusuableSeparator";
-export { default as ProductionEndCtaPage } from "./ProductionEndCtaPage";s
+export { default as ProductionEndCtaPage } from "./ProductionEndCtaPage";
