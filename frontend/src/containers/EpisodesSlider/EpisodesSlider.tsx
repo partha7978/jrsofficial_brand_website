cbcd5a8379a67627eb87db0fb8fc1de1615a69a2
Muduli, Partha <pmuduli@deloitte.com>
2025-05-07 13:26:35 +0530

    Fixed episode slider animation scroll issue

--- frontend/src/containers/EpisodesSlider/EpisodesSlider.tsx
+++ frontend/src/containers/EpisodesSlider/EpisodesSlider.tsx
@@ -86,8 +86,8 @@ const EpisodesSlider = () => {
                   showLatestResult.length > 0 &&
                   showLatestResult.map((episode, index) => (
                     <motion.div
-                      initial={{ opacity: 0, y: 50 }}
-                      whileInView={{ opacity: 1, y: 0 }}
+                      initial={{ opacity: 0, x: 30 }}
+                      whileInView={{ opacity: 1, x: 0 }}
                       transition={{
                         duration: 0.5,
                         ease: "backInOut",
