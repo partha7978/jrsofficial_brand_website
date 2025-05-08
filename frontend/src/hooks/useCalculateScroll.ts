19abcb116862f830505fa1b4acd1db98144fdca9
Muduli, Partha <pmuduli@deloitte.com>
2025-05-08 12:01:48 +0530

    Implemented throttle for calculate scroll and width.

--- frontend/src/hooks/useCalculateScroll.ts
+++ frontend/src/hooks/useCalculateScroll.ts
@@ -4,6 +4,31 @@ const useCalculateScrollAndWidth = () => {
   const [scrollYNumber, setScrollYNumber] = useState(0);
   const [innerWidth, setInnerWidth] = useState(window.innerWidth);
 
+  const throttle = (func: any, delay: number = 350) => {
+    let waitingArgs: any = null;
+    let shouldWait = false;
+
+    const timeoutFunc = () => {
+      if (waitingArgs == null) {
+        shouldWait = false;
+      } else {
+        func(...waitingArgs);
+        waitingArgs = null;
+        setTimeout(timeoutFunc, delay);
+      }
+    };
+    return (...args: any) => {
+      if (shouldWait) {
+        waitingArgs = args;
+        return;
+      }
+      func(...args);
+      shouldWait = true;
+
+      setTimeout(timeoutFunc, delay);
+    };
+  };
+
   useEffect(() => {
     const handleScroll = () => {
       requestAnimationFrame(() => setScrollYNumber(window.scrollY));
@@ -13,8 +38,10 @@ const useCalculateScrollAndWidth = () => {
       setInnerWidth(window.innerWidth);
     };
 
-    window.addEventListener("scroll", handleScroll, { passive: true });
-    window.addEventListener("resize", handleResize);
+    window.addEventListener("scroll", throttle(handleScroll), {
+      passive: true,
+    });
+    window.addEventListener("resize", throttle(handleResize));
 
     return () => {
       window.removeEventListener("scroll", handleScroll);
