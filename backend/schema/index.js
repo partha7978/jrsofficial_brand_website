75825fb43e2bf9edc750acfb7d07124ede752b30
Muduli, Partha <pmuduli@deloitte.com>
2025-05-12 13:42:47 +0530

    Updated homepage course schema.

--- backend/schema/index.js
+++ backend/schema/index.js
@@ -9,12 +9,14 @@ import course from './course'
 import userSubmittedData from './userSubmittedData'
 import production from './production'
 import videoTestimonial from './videoTestimonial'
+import homepageCourse from './homepageCourse'
 
 export const schemaTypes = [
   navigationBar,
   homePage,
   episodes,
   about,
+  homepageCourse,
   testimonial,
   contact,
   mainAboutPage,
